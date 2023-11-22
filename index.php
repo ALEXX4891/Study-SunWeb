a50f879f48421005ee71dbf4841e233d9a74c667
ALEX4891 <alexx4891@mail.ru>
2023-11-22 12:48:32 +0500

    fix name branches

--- index.php
+++ index.php
@@ -12,16 +12,16 @@
         <ul>
             <?php
                 function fibonacci($n,$first = 0,$second = 1) {
-                    echo('<li class="red-text">' . 0 . '</li>');
+                    print_r('<li>' . 0 . '</li>');
                     $fib = [$first,$second];
                     for($i=1;$i<$n;$i++)
                         {
                             $fib[] = $fib[$i]+$fib[$i-1];
-                            if ($i % 2 != 0) {
-                                echo('<li class="blue-text">' . $fib[$i] . '</li>');
+                            if ($fib[$i] % 2 == 0) {
+                                print_r('<li class="blue-text">' . $fib[$i] . '</li>');
                             } else {
                     
-                                echo('<li class="red-text">' . $fib[$i] . '</li>');    
+                                print_r('<li class="black-text">' . $fib[$i] . '</li>');    
                             }
                         }
                     return $fib;
